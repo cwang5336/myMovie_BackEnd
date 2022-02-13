aee5b328b23c455d255cb84ba272c54afdc7deb0
Cheng Wang <cheng@chengs-mbp.lan>
2022-02-12 23:26:57 -0500

    final.13

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const compression = require("compression");
 const bodyParser = require("body-parser"); 
 const db = require("./db"); 
 const PORT = process.env.PORT || 5432;
+
 app.listen(PORT, () => {
     console.log("server from port 5432");  //server running on port 5000
 });
